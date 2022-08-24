8b59bf1faa153e06fe71ad8d18819b41ed3b60fa
kmjang <kyumin.jang@gmail.com>
2022-08-24 15:33:39 +0900

    django tutorial step 3 완료

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+'''
+    path에 들어가는 converter는 int, path, slug, str, uuid를 사용 가능하다.
+'''
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
