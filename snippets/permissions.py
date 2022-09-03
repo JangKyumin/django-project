1e6d34c88124215a256418fd42cee2fb286ddc43
kmjang <kyumin.jang@gmail.com>
2022-09-03 23:28:44 +0900

    주석 추가

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -7,6 +7,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
+        # GET, HEAD, OPTIONS 이 세가지 메소드만 넘겨준다.
         if request.method in permissions.SAFE_METHODS:
             return True
 
